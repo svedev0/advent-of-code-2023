97293cb6383dcbdf3e06d0fe6d9ca6d88fd96fc3
svedev0 <emanuel@kulich.me>
2023-12-02 16:34:23 +0100

    Improved day handling

--- Program.cs
+++ Program.cs
@@ -1,7 +1,38 @@
-﻿using advent_of_code_2023;
+﻿namespace advent_of_code_2023;
 
-// Day01.SolvePart1();
-// Day01.SolvePart2();
+public class Program
+{
+	private static readonly int YEAR = 2023;
+	private static readonly int DAY = 2;
 
-// Day02.SolvePart1();
-Day02.SolvePart2();
+	public static void Main(string[] _)
+	{
+		Welcome();
+
+		switch (DAY)
+		{
+			case 1:
+				Day01.SolvePart1();
+				Day01.SolvePart2();
+				break;
+			case 2:
+				Day02.SolvePart1();
+				Day02.SolvePart2();
+				break;
+			default:
+				Console.WriteLine("Invalid day");
+				break;
+		}
+	}
+
+	private static void Welcome()
+	{
+		string messsage = $"""
+			===== Advent of Code {YEAR} =====
+
+			Day {DAY}:
+
+			""";
+		Console.WriteLine(messsage);
+	}
+}
