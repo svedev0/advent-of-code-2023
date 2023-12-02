e5d28931f7a148d936522635b24e5aff3b93f354
svedev0 <emanuel@kulich.me>
2023-12-02 14:10:32 +0100

    Day02 part 1

---
+++ Day02/Day02.cs
@@ -0,0 +1,47 @@
+﻿namespace advent_of_code_2023;
+
+public class Day02
+{
+	public static void SolvePart1()
+	{
+		List<string> input = [.. File.ReadAllLines("Day02\\input.txt")];
+		Dictionary<string, int> maximums = new()
+		{
+			{ "red",   12 },
+			{ "green", 13 },
+			{ "blue",  14 },
+		};
+		List<int> possibleGames = [];
+
+		for (int i = 0; i < input.Count; i++)
+		{
+			int gameNum = i + 1;
+			bool gamePossible = true;
+
+			string results = input[i].Split(": ")[1];
+			List<string> rounds = [.. results.Split("; ")];
+			
+			foreach (var round in rounds)
+			{
+				foreach (var cubes in round.Split(", "))
+				{
+					int count = int.Parse(cubes.Split(" ")[0]);
+					string colour = cubes.Split(" ")[1];
+
+					if (count > maximums[colour])
+					{
+						gamePossible = false;
+						continue;
+					}
+				}
+			}
+
+			if (gamePossible)
+			{
+				possibleGames.Add(gameNum);
+			}
+		}
+
+		Console.WriteLine($"Sum of possible games: {possibleGames.Sum()}");
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using advent_of_code_2023;
 
-Day01.SolvePart1();
-Day01.SolvePart2();
+// Day01.SolvePart1();
+// Day01.SolvePart2();
+Day02.SolvePart1();
